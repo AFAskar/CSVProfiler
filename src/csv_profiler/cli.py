f7f9b5755bd9c3cf307695f06315e613ebbc6c14
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2025-12-18 16:02:16 +0300

    Match client reqs

--- src/csv_profiler/cli.py
+++ src/csv_profiler/cli.py
@@ -12,31 +12,47 @@ app = typer.Typer()
 def cli(
     input: Path = typer.Argument(..., exists=True, file_okay=True, dir_okay=False),
     output: Path | None = typer.Option(
-        None,
+        Path("outputs"),
         "-o",
-        "--output",
-        help="Output report file path. If not provided, defaults to 'Report.md'",
+        "--out-dir",
+        help="Output report directory path. If not provided, defaults to 'outputs'",
+    ),
+    format: str = typer.Option(
+        "both",
+        "-f",
+        "--format",
+        help="Output report format. Supported formats are 'md' 'json', and both. Defaults to 'both'",
     ),
 ):
     if input.suffix.lower() != ".csv":
         raise click.ClickException("Input file must be a CSV file.")
     csvfile = read_csv(input)
     data = profiler(csvfile)
-    if output is None:
-        output_path = Path("Report.md")
-        print(f"No output path provided. Using default: {output_path}")
-    else:
-        output_path = output
-    output_ext = Path(output_path).suffix.lower()
-    if output_ext == ".md":
+    output.mkdir(parents=True, exist_ok=True)
+    output_ext = format.lower()
+
+    if output_ext == "both":
+        md_path = output / "report.md"
+        json_path = output / "report.json"
+        md_text = createMD(data)
+        json_text = createJsonReport(data)
+        md_path.write_text(md_text)
+        json_path.write_text(json_text)
+        print(f"[bold green]Reports generated:[/bold green] {md_path}, {json_path}")
+    if output_ext == "md":
+        md_path = output / "report.md"
         text = createMD(data)
-        output_path.write_text(text)
-    elif output_ext == ".json":
+        md_path.write_text(text)
+        print(f"[bold green]Markdown report generated:[/bold green] {md_path}")
+    if output_ext == "json":
+        json_path = output / "report.json"
         text = createJsonReport(data)
-        output_path.write_text(text)
+        json_path.write_text(text)
+        print(f"[bold green]JSON report generated:[/bold green] {json_path}")
+
     else:
         raise click.ClickException(
-            "Unsupported output file format. Please use .md or .json"
+            "Unsupported output file format. Please use 'md', 'json', or 'both'"
         )
 
     return
